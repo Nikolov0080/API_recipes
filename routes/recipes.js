1cf9aa6b0a2fb6a08a207098390e72e901759bd3
Nikolov0080 <apofis12343@gmail.com>
2020-09-28 20:03:48 +0300

    created: testing form -create_recipe

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/create-recipe',(req,res)=>{
+router.get('/create-recipe', (req, res) => {
     res.render('createRecipe')
+}).post('/create-recipe', (req, res) => {
+    console.log(req.body)
+    res.redirect('/recipes/create-recipe');
 })
 
 module.exports = router;
