2b09ff7b713bd552542489d694f7a408e45d4496
Nikolov0080 <apofis12343@gmail.com>
2020-10-06 21:51:30 +0300

    updated details

--- controllers/recipes/getRecipeDetails.js
+++ controllers/recipes/getRecipeDetails.js
@@ -2,16 +2,17 @@ const recipeSchema = require('../../models/recipes/recipeSchema');
 
 module.exports.getRecipeDetails = (req, res) => {
 
+console.log(req.url)
+
     // const id = req.params / req.body
     const testId = "5f7b622a18e4fe41b8b7392e"
-    const recipe = recipeSchema.findById(testId, (err, data) => {
+    recipeSchema.findById(testId, (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
         return data;
     }).exec().then((data) => {
-
         res.render('recipeDetails', { data:data,prepTime:data.prepTime });
     })
 
