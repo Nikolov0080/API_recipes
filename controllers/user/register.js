b86e24753888d2ef6e9e3238ac65dd8b6aee5d1a
Nikolov0080 <apofis12343@gmail.com>
2020-10-10 17:45:40 +0300

    todo - structure error arrays from login and register

--- controllers/user/register.js
+++ controllers/user/register.js
@@ -11,9 +11,11 @@ module.exports.registerGet = (req, res) => {
 
 module.exports.registerPost = (req, res) => {
 
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+
+console.log(errors.array())
+        
         return res.status(400).json({ errors: errors.array() });
     }
 
